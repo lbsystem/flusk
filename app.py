563a3de3f22fe8582855966747da144192141690
lbsystem <lbsystem@126.com>
2022-05-24 19:00:10 +0800

    company flusk

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-import json,os
+import json, os
 
-from flask import Flask, request, render_template, jsonify,make_response,session
+from flask import Flask, request, render_template, jsonify, make_response, session,send_from_directory
 from pymongo import MongoClient
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
@@ -8,35 +8,33 @@ from Crypto.Util.Padding import pad
 import base64
 import hashlib
 
-SECRET_KEY='fasdfasdqwrqwe'
+SECRET_KEY = 'fasdfasdqwrqwe'
 
 client = MongoClient(host="192.168.1.32", port=27017)
 collections = client["javbus"]["company"]
 
+
 def en_aes(data):
+    key = b"1111111111111111"
+    aes = AES.new(key=key, IV=b'1111111111111111', mode=AES.MODE_CBC)
 
+    data = data
+    data = pad(data.encode(), 16, style='pkcs7')
 
-    key=b"1111111111111111"
-    aes=AES.new(key=key,IV=b'1111111111111111',mode=AES.MODE_CBC)
-
-    data=data
-    data=pad(data.encode(),16,style='pkcs7')
-
-    bs=aes.encrypt(data)
+    bs = aes.encrypt(data)
     # data=aes_decrypt(bs)
     print(bs)
-    bs=base64.b64encode(bs)
-    return  bs.decode()
+    bs = base64.b64encode(bs)
+    return bs.decode()
 
 
 def aes_decrypt(data):
-
-    key=b"1111111111111111"
-    data=base64.b64decode(data.encode())
-    de_aes=AES.new(key=key,IV=b'1111111111111111',mode=AES.MODE_CBC)
-    data=de_aes.decrypt(data)
-    data=unpad(data,16,style="pkcs7")
-    data=data.decode()
+    key = b"1111111111111111"
+    data = base64.b64decode(data.encode())
+    de_aes = AES.new(key=key, IV=b'1111111111111111', mode=AES.MODE_CBC)
+    data = de_aes.decrypt(data)
+    data = unpad(data, 16, style="pkcs7")
+    data = data.decode()
     return data
 
 
@@ -44,7 +42,9 @@ app = Flask(__name__)
 
 print(__name__)
 
-app.config['SECRET_KEY']="dadasfgafdaf"
+app.config['SECRET_KEY'] = "dadasfgafdaf"
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     item = {}
@@ -52,25 +52,22 @@ def index():
     item['age'] = 18
     item['all'] = 'sdfasdggasdgasdgaserwerwq'
     if request.method == "GET":
-        session["session"]="test session"
-        response=make_response(render_template('index.html'))
-        response.set_cookie("name","lbsystem")
-
-
+        session["session"] = "test session"
+        response = make_response(render_template('index.html'))
+        response.set_cookie("name", "lbsystem")
 
         return response
     else:
         res = request.form.to_dict()
         print(res)
-        data=aes_decrypt(res['src'])
-        status=hashlib.sha1(data.encode()).hexdigest()
+        data = aes_decrypt(res['src'])
+        status = hashlib.sha1(data.encode()).hexdigest()
         print(status)
-        if status==res['hash']:
-
+        if status == res['hash']:
             print("confire")
         print(data)
         # res = collections.find_one({'code': res['src']}, {"_id": 0})
-        item['encode']=en_aes(json.dumps(item))
+        item['encode'] = en_aes(json.dumps(item))
         print(item['encode'])
         print(hashlib.sha256("pp2323pppp".encode()).hexdigest())
         return jsonify(item)
@@ -91,6 +88,7 @@ def index():
         #     print(request.data)
         #
 
+
 # @app.after_request
 # def check():
 #     print(Response.headers)
@@ -100,21 +98,29 @@ def index():
 def test():
     return render_template('testscroll.html')
 
-@app.route("/img",methods=['POST'])
+
+@app.route("/img", methods=['POST'])
 def get_img():
     # res = collections.find_one({}, {"code":1,"_id": 0})
-    res=os.listdir('./static/img')
+    res = os.listdir('./static/img')
     return jsonify(res)
+
 
 @app.route("/js")
 def js():
     return render_template('js.html')
 
 
+@app.route("/ajax", methods=["GET", "POST"])
+def ajax():
+    resp = make_response(send_from_directory(path="./static/video/a.mkv",directory='./static/video',filename="a.mkv",as_attachment=True))
+    # resp =make_response("fasdfas")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET,POST'
+    resp.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    resp.headers["Content-Disposition"]="attachment;filename=FileName.mkv"
+    print(request.form.to_dict())
+    return resp
 
-
-
-
-
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',port=81)
--- pytest.py
+++ pytest.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 import os
 
+l=[12,2,3,4,66,3,22,8]
 
-list1=os.listdir('./static/img')
+d=dict(
+    a=6,
+    b=9,
+    c=10,
+    d=3,
+    e=7,
+    f=8
+)
+print(os.path.abspath("/static/video"))
 
-print(list1)
+result=filter(lambda x:x>8,d.values())
+print(list(result))
